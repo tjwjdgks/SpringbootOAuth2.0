702fcf91f5c28c5911847d36fe63130fd733b6c3
tjwjdgks <tjwjdgks43@gmail.com>
2022-01-02 13:17:35 +0900

    test

---
+++ src/main/java/com/example/oauth2study/OAuth2StudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2StudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2StudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oauth2study/OAuth2StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth2study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2StudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
